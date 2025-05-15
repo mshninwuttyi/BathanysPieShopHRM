1ebebd693537f765102b0f31e36ef547bc32143d
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-05-15 12:09:50 +0700

    feat: Explore about string and int

--- BathanysPieShopHRM/Program.cs
+++ BathanysPieShopHRM/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//string
+string name2 = Console.ReadLine();
+string name_2 = Console.ReadLine();
+string Name_2 = Console.ReadLine();
+//string 2Name = Console.ReadLine();
+
+
+//int
+int monthlyWage = 1234;
+int month = 12, bones = 1000;
